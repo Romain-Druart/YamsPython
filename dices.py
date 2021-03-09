9b7b6d0f543a99ca3b7129af2e28535fa8b31870
Romain-Druart <druart.romainpro@gmail.com>
2021-03-09 10:30:42 +0100

    Ajout de fichiers

---
+++ dices.py
@@ -0,0 +1,27 @@
+import random
+
+class Dice():
+
+    dices = []
+
+    def __init__(self, *args, **kwargs):
+        for i in range(1,6):
+            dices.append(random.randint(1,6))
+
+    def suffleDices(dices):
+
+            
+
+#import random
+
+
+#def my_function(food):
+#  for i in dices:
+#  	print(i)
+
+#dices = []
+
+#for i in range(1,6):
+#  	dices.append(random.randint(1,6))
+
+#my_function(dices)
--- gameRules.py
+++ gameRules.py
@@ -1,1 +1,153 @@
+#Game rules : Checking dices; Winning player;
+class GameRules():
 
+
+    def n1(dices: list) -> int:
+        """ Number of one """
+        score = 0
+        for i in range(5):
+            if dices[i] == 1:
+                score = score + dices[i]
+        return score
+     
+ 
+    def n2(dices: list) -> int:
+        """ Number of two """
+        score = 0
+        for i in range(5):
+            if dices[i] == 2:
+                score = score + dices[i]
+        return score
+     
+ 
+    def n3(dices: list) -> int:
+        """ Number of three """
+        score = 0
+        for i in range(5):
+            if dices[i] == 3:
+                score = score + dices[i]
+        return score
+     
+ 
+    def n4(dices: list) -> int:
+        """ Number of four """
+        score = 0
+        for i in range(5):
+            if dices[i] == 4:
+                score = score + dices[i]
+        return score
+     
+ 
+    def n5(dices: list) -> int:
+        """ Number of five """
+        score = 0
+        for i in range(5):
+            if dices[i] == 5:
+                score = score + dices[i]
+        return score
+     
+ 
+    def n6(dices: list) -> int:
+        """ Number of six """
+        score = 0
+        for i in range(5):
+            if dices[i] == 6:
+                score = score + dices[i]
+        return score
+     
+ 
+    def n3_oak(dices: list) -> int:
+        """ Check for three-of-a-kind """
+        sorted_dices = sorted(dices)
+        count = 1
+        for i in range(1, 5):
+            if sorted_dices[i] == sorted_dices[i - 1]:
+                count = count + 1
+            else:
+                count = 1
+         
+            if count == 3:
+                return sum(dices)
+        return 0
+ 
+ 
+    def n4_oak(dices: list) -> int:
+        """ Check for four-of-a-kind """
+        sorted_dices = sorted(dices)
+        count = 1
+        for i in range(1, 5):
+            if sorted_dices[i] == sorted_dices[i - 1]:
+                count = count + 1
+            else:
+                count = 1
+         
+            if count == 4:
+                return 40 + sum(dices)
+ 
+        return 0
+ 
+ 
+    def full(dices: list) -> int:
+        """ Check for full """
+        sorted_dices = sorted(dices)
+        count = 1
+        two, three = False, False
+        for i in range(1, 5):
+            if sorted_dices[i] == sorted_dices[i - 1]:
+                count = count + 1
+            else:
+                count = 1
+ 
+            if count == 2:
+                two = True
+            if count == 3:
+                three = True
+ 
+        if two and three:
+            return 25 + sum(dices)
+        return 0
+ 
+ 
+    def small_s(dices: list) -> int:
+        """ Check for sequences of length 4 """
+        sorted_dices = sorted(dices)
+        count = 1
+        for i in range(1, 5):
+            if sorted_dices[i] == sorted_dices[i - 1] + 1:
+                count = count + 1
+            elif sorted_dices[i] != sorted_dices[i - 1]:
+                count = 1
+            if count == 4:
+                return 30
+     
+        return 0
+ 
+ 
+    def large_s(dices: list) -> int:
+        """ Check for sequences of length 5 """
+        sorted_dices = sorted(dices)
+        count = 1
+        for i in range(1, 5):
+            if sorted_dices[i] == sorted_dices[i - 1] + 1:
+                count = count + 1
+            elif sorted_dices[i] != sorted_dices[i - 1]:
+                count = 1
+ 
+            if count == 5:
+                return 40
+ 
+        return 0
+ 
+ 
+    def yahtzee(dices: list) -> int:
+        """ Check for 5 same dices """
+        for i in range(1, 5):
+            if dices[i] != dices[i - 1]:
+                return 0
+ 
+        return 50 + sum(dices)
+ 
+ 
+    def chance(dices: list) -> int:
+        """ Actually returns the sum of dices """
+        return sum(dices)
